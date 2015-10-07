3525e9379121e42fc6b10a2e5e06250e428324c3
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-07 14:57:50 +0200

    more tests

--- tests/test_omitempty.py
+++ tests/test_omitempty.py
@@ -9,7 +9,35 @@ else:
 
 import omitempty
 
+class EmptyObj(object):
+    def __nonzero__(self):
+        return False
+
+class ZeroLenObj(object):
+    def __len__(self):
+        return 0
+
 class TestOmitempty(unittest.TestCase):
 
     def test_empty(self):
         self.assertEquals({}, omitempty({}))
+
+    def test_empty_values(self):
+        self.assertEquals({}, omitempty({
+            "x": 0,
+            "y": [],
+            "z": False,
+            3: {},
+            5: (),
+            6: EmptyObj(),
+            4: ZeroLenObj(),
+        }))
+
+    def test_simple_rec(self):
+        d = {}
+        d["x"] = d
+
+        self.assertEquals(d, omitempty(d))
+
+    def test_simple(self):
+        self.assertEquals({"a": 1, "c": 3}, omitempty({"a": 1, "b": 0, "c": 3}))
